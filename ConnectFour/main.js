6878bcb346e89e35b3fee70400ecc9b281e29f90
peterbucci <sb1285n@gmail.com>
2018-11-03 20:23:59 -0400

    working game

--- ConnectFour/main.js
+++ ConnectFour/main.js
@@ -1,6 +1,4 @@
 // Create Gameboard
-
-let board = []
 let aboveBoard = document.getElementById('aboveBoard')
 let gameBoard = document.getElementById('gameBoard')
 let afterBoard = document.getElementById('afterBoard')
@@ -37,15 +35,9 @@ class Column {
 		}
 	}
 
-	removeEvent () {
-			this.el.removeEventListener('click', this.onClick.bind(this))
-			this.el.removeEventListener('mouseenter', this.onMouseEnter.bind(this))
-	}
-
 	onMouseEnter () {
 		if (!game.disabled) {
-			let chip = document.getElementById('chip')
-			chip.offsetTop == 0 && (chip.style.left = `${10 * this.index}vh`)
+			game.chip.el.offsetTop == 0 && (game.chip.el.style.left = `${10 * this.index}vh`)
 		}
 	}
 
@@ -53,33 +45,36 @@ class Column {
 		let availableSquares = this.squares.filter(function(square) {
 			return square.fill == 0
 		})
-		if (!game.disabled && chip.el.offsetTop == 0 && availableSquares.length > 0) {
+		if (!game.disabled && game.chip.el.offsetTop == 0 && availableSquares.length > 0) {
 			game.disabled = true
 
 			function click () {
 				let firstAvailable = availableSquares[availableSquares.length - 1]
-				chip.el.style.top = firstAvailable.el.offsetTop + chip.el.clientHeight + 'px'
+				game.chip.el.style.top = firstAvailable.el.offsetTop + game.chip.el.clientHeight + 'px'
 
 				setTimeout(function() {
 					if (game.turn % 2 == 0) {
 						firstAvailable.el.style.backgroundColor = game.players[1].color
 						firstAvailable.fill = 2
+						game.chip.deleteEl()
+						game.chip = new Chip(aboveBoard, game.players[0].color)
 					} else {
 						firstAvailable.el.style.backgroundColor = game.players[0].color
 						firstAvailable.fill = 1
+						game.chip.deleteEl()
+						game.chip = new Chip(aboveBoard, game.players[1].color)
 					}
 					
 					game.lastMove = firstAvailable
 					game.takeTurn()
-					chip.refreshEl()
 					!game.over && (game.disabled = false)
 				}, 1000)
 			}
 
-			if (chip.el.offsetLeft !== this.el.offsetLeft) {
-				chip.el.style.left = `${10 * this.index}vh`
+			if (game.chip.el.offsetLeft !== this.el.offsetLeft) {
+				game.chip.el.style.left = `${10 * this.index}vh`
 			
-				chip.el.addEventListener('transitionend', click, {once: true})
+				game.chip.el.addEventListener('transitionend', click, {once: true})
 			} else {
 				click()
 			}
@@ -87,36 +82,20 @@ class Column {
 	}
 }
 
-for (let i = 0; i < 7; i++) {
-	let column = new Column(i)
-	gameBoard.appendChild(column.el)
-	board.push(column)
-}
-
 // Create Chip
 
 class Chip {
-	constructor (parent) {
+	constructor (parent, color) {
 		this.parent = parent
-		this.createEl()
+		this.createEl(color)
 	}
 
-	refreshEl () {
-		this.el.remove()
-		!game.over && this.createEl()
-		!game.over && this.appendEl()
-	}
-
-	createEl () {
+	createEl (color) {
 		let el = document.createElement('div')
 		el.setAttribute('id', 'chip')
-
 		this.el = el
-	}
-
-	appendEl () {
-		game.turn % 2 == 0 ? this.el.style.backgroundColor = game.players[1].color : this.el.style.backgroundColor = game.players[0].color
 		this.parent.appendChild(this.el)
+		this.el.style.backgroundColor = color
 		setTimeout(() => {
 			this.el.style.top = '0' 
 			this.el.style.left = '0'
@@ -124,14 +103,14 @@ class Chip {
 	}
 
 	deleteEl () {
-		this.el.remove()
+		game.chip.el.remove()
 	}
 }
 
 // Start Game
 
 class Player {
-	constructor (name, color, card) {
+	constructor (name, color) {
 		this.name = name
 		this.color = color
 		this.turn = name + '\'s move'
@@ -142,108 +121,158 @@ class Game {
 	constructor () {
 		this.players = []
 		this.turn = 1
-		this.disabled = true
-		this.over = false
 		this.lastMove
+		this.chip
+		// If true, disable event listener functions
+		this.disabled = true
+		// If true, game is over
+		this.over = false
 		
-		// Game UI (The current turn and whose move it is)
-		
-		let moveUI = document.createElement('div')
-		let turnUI = document.createElement('div')
-		
-		moveUI.setAttribute('id', 'moveUI')
-		afterBoard.insertAdjacentElement('afterbegin', moveUI)
-		turnUI.setAttribute('id', 'turnUI')
-		afterBoard.insertAdjacentElement('beforeend', turnUI)
+		this.toggleBoard()
+		this.toggleGameUI()
+		this.toggleCreateACharacterUI()
+	}
 
-		this.whoseMove = moveUI
-		this.currentTurn = turnUI
-		
-		// Character Create UI
-		
-		let createACharacterUI = document.createElement('div')
-		createACharacterUI.setAttribute('class', 'createACharacterUI')
-		afterBoard.insertAdjacentElement('beforeend', createACharacterUI)
-	
-		let nameInput = document.createElement('input')
-		nameInput.setAttribute('id', 'nameInput')
-		nameInput.setAttribute('value', 'Enter A Name')
-		
-		let instructions = document.createElement('span')
-		instructions.setAttribute('id', 'instructions')
-		let text = 'Player One'
-		instructions.innerHTML = text
-	
-		let chooseRed = document.createElement('div')
-		let chooseBlack = document.createElement('div')
-		chooseRed.setAttribute('class', 'pickAColor')
-		chooseRed.setAttribute('id', 'red')
-		chooseBlack.setAttribute('class', 'pickAColor')
-		chooseBlack.setAttribute('id', 'black')
+	toggleBoard () {
 
-		let submit = document.createElement('button')
-		submit.innerHTML = 'Submit'
-	
-		let construct = [instructions, nameInput, [chooseRed, chooseBlack], submit]
-	
-		construct.forEach((el) => {
-			let containers = document.createElement('div')
-			containers.setAttribute('class', 'pContainer')
-			createACharacterUI.insertAdjacentElement('beforeend', containers)
-	
-			if (el.length == 2) {
-				containers.appendChild(el[0])
-				containers.appendChild(el[1])
-			} else {
-				containers.appendChild(el)
+		if (!this.board) {
+			this.board = []
+			for (let i = 0; i < 7; i++) {
+				let column = new Column(i)
+				gameBoard.appendChild(column.el)
+				this.board.push(column)
 			}
-		})
+		} else {
+			this.board.forEach((column) => {
+				column.el.remove()
+			})
+		}
+	}
 
-		function addPlayer () {
-			if (this.color && nameInput.value.length > 3) {
-				let player = new Player(nameInput.value, this.color.id)
-				this.players.push(player)
-				
-				this.color.remove()
-				instructions.innerHTML = 'Player Two'
-				nameInput.setAttribute('value', 'Enter A Name')
-				this.color = null
-				
-				if (this.players.length == 2) {
-					createACharacterUI.remove()
+	toggleCreateACharacterUI () {
+		if (!this.createACharacterUI) {
+			this.createACharacterUI = document.createElement('div')
+			this.createACharacterUI.setAttribute('class', 'createACharacterUI')
+			afterBoard.insertAdjacentElement('beforeend', this.createACharacterUI)
 
-					this.whoseMove.innerHTML = game.players[0].turn
+			let title = document.createElement('span')
+			title.setAttribute('id', 'title')
+			let text = 'Connect'
+			title.innerHTML = text
+
+			let titleNum = document.createElement('span')
+			titleNum.setAttribute('id', 'titleNum')
+			text = '4'
+			titleNum.innerHTML = text
 		
-					this.currentTurn.innerHTML = 'Turn: ' + game.turn.toString()
-					chip.appendEl()
-					this.disabled = false
+			let nameInput = document.createElement('input')
+			nameInput.setAttribute('id', 'nameInput')
+			nameInput.setAttribute('value', 'Enter A Name')
+			
+			let instructions = document.createElement('span')
+			instructions.setAttribute('id', 'instructions')
+			text = 'Player One'
+			instructions.innerHTML = text
+		
+			let chooseRed = document.createElement('div')
+			let chooseBlack = document.createElement('div')
+			chooseRed.setAttribute('class', 'pickAColor')
+			chooseRed.setAttribute('id', 'red')
+			chooseBlack.setAttribute('class', 'pickAColor')
+			chooseBlack.setAttribute('id', 'black')
+
+			let submit = document.createElement('button')
+			submit.innerHTML = 'Submit'
+		
+			let construct = [[title, titleNum], instructions, nameInput, [chooseRed, chooseBlack], submit]
+		
+			construct.forEach((el) => {
+				let containers = document.createElement('div')
+				containers.setAttribute('class', 'pContainer')
+				this.createACharacterUI.insertAdjacentElement('beforeend', containers)
+		
+				if (el.length == 2) {
+					containers.appendChild(el[0])
+					containers.appendChild(el[1])
+				} else {
+					containers.appendChild(el)
 				}
-			}	
-		}
+			})
 
-		this.color = null
+			function addPlayer () {
+				if (this.color && nameInput.value.length > 3) {
+					let player = new Player(nameInput.value, this.color.id)
+					this.players.push(player)
+					
+					this.color.remove()
+					instructions.innerHTML = 'Player Two'
+					nameInput.setAttribute('value', 'Enter A Name')
+					this.color = null
+					
+					if (this.players.length == 2) {
+						this.toggleCreateACharacterUI()
 
-		function chooseAColor (e) {
-			if (e.target.style.boxShadow === 'rgb(66, 66, 66) 5px 5px 5px') {
-				e.target.style.boxShadow = 'none'
-				this.color = null
-			} else {
-				this.color = e.target
-				this.color.style.boxShadow = 'rgb(66, 66, 66) 5px 5px 5px'
-				this.color.id === 'red' && this.color.nextSibling && (this.color.nextSibling.style.boxShadow = 'none')
-				this.color.id === 'black' && this.color.previousSibling && (this.color.previousSibling.style.boxShadow = 'none')
+						this.moveUI.innerHTML = game.players[0].turn
+			
+						this.turnUI.innerHTML = 'Turn: ' + game.turn.toString()
+						this.chip = new Chip(aboveBoard, this.players[0].color)
+						this.disabled = false
+					}
+				}	
 			}
+
+			this.color = null
+
+			function chooseAColor (e) {
+				if (e.target.style.boxShadow === 'rgb(66, 66, 66) 5px 5px 5px') {
+					e.target.style.boxShadow = 'none'
+					this.color = null
+				} else {
+					this.color = e.target
+					this.color.style.boxShadow = 'rgb(66, 66, 66) 5px 5px 5px'
+					this.color.id === 'red' && this.color.nextSibling && (this.color.nextSibling.style.boxShadow = 'none')
+					this.color.id === 'black' && this.color.previousSibling && (this.color.previousSibling.style.boxShadow = 'none')
+				}
+			}
+
+			chooseRed.addEventListener('click', chooseAColor.bind(this))
+			chooseBlack.addEventListener('click', chooseAColor.bind(this))
+
+			submit.addEventListener('click', addPlayer.bind(this))
+		} else {
+			this.createACharacterUI.remove()
 		}
+	}
 
-		chooseRed.addEventListener('click', chooseAColor.bind(this))
-		chooseBlack.addEventListener('click', chooseAColor.bind(this))
+	toggleGameUI () {
+		if (!this.moveUI && !this.turnUI) {
+			this.moveUI = document.createElement('div')
+			this.turnUI = document.createElement('div')
+			
+			this.moveUI.setAttribute('id', 'moveUI')
+			afterBoard.insertAdjacentElement('afterbegin', this.moveUI)
+			this.turnUI.setAttribute('id', 'turnUI')
+			afterBoard.insertAdjacentElement('beforeend', this.turnUI)
+		} else {
+			this.moveUI.remove()
+			this.turnUI.remove()
+		}
+	}
 
-		submit.addEventListener('click', addPlayer.bind(this))
-
-		// Win Screen
-		this.winScreen = document.createElement('div')
-		this.winScreen.setAttribute('id', 'winScreen')
-
+	toggleWinScreen (winner) {
+		if (!this.winScreen) {
+			this.winScreen = document.createElement('div')
+			this.winScreen.setAttribute('id', 'winScreen')
+			this.winScreen.insertAdjacentHTML('afterbegin', winner + ' wins!')
+			let newGame = document.createElement('button')
+			newGame.setAttribute('id', 'newGame')
+			newGame.innerHTML = 'New Game'
+			this.winScreen.insertAdjacentElement('beforeend', newGame)
+	
+			this.winScreen.addEventListener('click', this.newGame.bind(this))
+		} else {
+			this.winScreen.remove()
+		}
 	}
 
 	takeTurn () {
@@ -270,12 +299,12 @@ class Game {
 			this.turn++
 
 			if (game.turn % 2 == 0) {
-				this.whoseMove.innerHTML = game.players[1].turn
+				this.moveUI.innerHTML = game.players[1].turn
 			} else {
-				this.whoseMove.innerHTML = game.players[0].turn
+				this.moveUI.innerHTML = game.players[0].turn
 			}
 
-			this.currentTurn.innerHTML = 'Turn: ' + game.turn.toString()
+			this.turnUI.innerHTML = 'Turn: ' + game.turn.toString()
 		}
 
 	}
@@ -283,19 +312,19 @@ class Game {
 	checkWinner (up, down, left, right, nextSquare, lastMove = this.lastMove, count = 1) {
 
 		if (up && nextSquare) {
-			board[nextSquare.column].squares[nextSquare.row - 1] ? nextSquare = board[nextSquare.column].squares[nextSquare.row - 1] 
+			this.board[nextSquare.column].squares[nextSquare.row - 1] ? nextSquare = this.board[nextSquare.column].squares[nextSquare.row - 1] 
 				: nextSquare = null
 		}
 		if (down && nextSquare) {
-			board[nextSquare.column].squares[nextSquare.row + 1] ? nextSquare = board[nextSquare.column].squares[nextSquare.row + 1]
+			this.board[nextSquare.column].squares[nextSquare.row + 1] ? nextSquare = this.board[nextSquare.column].squares[nextSquare.row + 1]
 				: nextSquare = null
 		}
 		if (left && nextSquare) {
-			board[nextSquare.column - 1] ? nextSquare = board[nextSquare.column - 1].squares[nextSquare.row]
+			this.board[nextSquare.column - 1] ? nextSquare = this.board[nextSquare.column - 1].squares[nextSquare.row]
 				: nextSquare = null
 		}
 		if (right && nextSquare) {
-			board[nextSquare.column + 1] ? nextSquare = board[nextSquare.column + 1].squares[nextSquare.row]
+			this.board[nextSquare.column + 1] ? nextSquare = this.board[nextSquare.column + 1].squares[nextSquare.row]
 				: nextSquare = null
 		}
 
@@ -309,17 +338,23 @@ class Game {
 	
 	winner () {
 		if (game.turn % 2 == 0) {
-			this.whoseMove.innerHTML = game.players[1].name + ' wins!'
-			this.winScreen.innerHTML = game.players[1].name + ' wins!'
+			this.moveUI.innerHTML = game.players[1].name + ' wins!'
+			this.toggleWinScreen(game.players[1].name)
 		} else {
-			this.whoseMove.innerHTML = game.players[0].name + ' wins!'
-			this.winScreen.innerHTML = game.players[1].name + ' wins!'
+			this.moveUI.innerHTML = game.players[0].name + ' wins!'
+			this.toggleWinScreen(game.players[0].name)
 		}
 		afterBoard.insertAdjacentElement('beforeend', this.winScreen)
 		this.over = true
 	}
+
+	newGame () {
+		this.toggleBoard()
+		this.toggleGameUI()
+		this.toggleWinScreen()
+		this.chip.deleteEl()
+		game = new Game()
+	}
 }
 
-let game = new Game()
-
-let chip = new Chip(aboveBoard)
+var game = new Game()
